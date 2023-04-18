34d9cdae301fbe568da9bac800c143da3a153dbe
Arturo <cf22arturo.vegas@iesjoandaustria.org>
2023-04-18 12:55:13 +0200

    sqlite

--- api_tasques.py
+++ api_tasques.py
@@ -8,7 +8,12 @@ import json
 app = flask.Flask(__name__)
 core_app = app_tasques.App_tasques()
 
-@app.route("/tasks", methods=["POST", "GET"])
+@app.route("/tasks/<id>", methods=["DELETE"])
+def delete_task(id):
+    resutat = core_app.esborra_tasca(id)
+    return "", 204
+
+@app.route("/tasks", methods=["POST", "GET", "PUT"])
 def tasks():
     if flask.request.method == "POST":
         info_body = flask.request.get.data()
@@ -16,6 +21,12 @@ def tasks():
         objecte_tasca = tasca.Tasca(None, tasca_nova["title"])
         resultat = core_app.afegueix_tasca(objecte_tasca)
         return "", 201
+    elif flask.request.method == "PUT":
+        info_body = flask.request.get.data()
+        tasca_nova = json.loads(info_body)
+        objecte_tasca = tasca.Tasca(None, tasca_nova["title"], tasca_nova["done"], tasca_nova["id"])
+        resultat = core_app.modifica_tasca(objecte_tasca)
+        return "", 204
     elif flask.request.method == "GET":
         llista_jsons = []
         llista_tasques = core_app.llegir_tasques()
